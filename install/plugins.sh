596eedd52c357937734b60014296061215a6d27d
Avanttic <Avanttic@users.noreply.github.com>
2017-02-21 16:52:48 +0100

    Update plugins.sh

--- install/plugins.sh
+++ install/plugins.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 v_dir=`dirname "$0"`
-mkdir /var/lib/jenkins/plugins/
+if [ ! -d /var/lib/jenkins/plugins/ ]
+then
+  mkdir /var/lib/jenkins/plugins/
+fi
+
 cd /var/lib/jenkins/plugins/
 
-while read plugin
+cat $v_dir/plugins.txt | while read plugin
 do
   wget "http://updates.jenkins-ci.org/latest/"$plugin".hpi"
-done < <(cat $v_dir/plugins.txt)
+done
+
+chown -R jenkins. /var/lib/jenkins/plugins/
